28f76384d378c673d6ee20dd3b7f12e2621c77a8
Unknown <gaz492@gmail.com>
2018-09-09 11:56:15 +0100

    config example

---
+++ config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+    curseAPI: {
+        username: "johnDoe@example.com",
+        password: "password123"
+    },
+    proxyAuth:{
+        enabled: true,
+        tokens: ['anarray of strings']
+    }
+};
