5b703413c24c73e7df95d2acdb6b96c5bdcaa1e4
Unknown <gaz492@gmail.com>
2018-09-09 11:54:48 +0100

    Init

---
+++ routes/curse.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const CAW = require('curse-api-wrapper');
+const config = require('../config');
+
+const curse = new CAW({username: config.curseAPI.username, password: config.curseAPI.password});
+curse.Authenticate();
+
+function checkToken(req, res, next) {
+    if(config.proxyAuth.enabled){
+        if(config.proxyAuth.tokens.includes(req.get('authToken'))){
+            next()
+        }else{
+            res.status(401);
+            res.send('Unauthorized');
+        }
+    }else{
+        next();
+    }
+}
+
+/* GET users listing. */
+router.post('/fingerprint', checkToken ,function (req, res, next) {
+    // console.log(req.body);
+    curse.GetFingerprintMatches(req.body)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.send(`Error handling request, server responded with: ${err}`)
+        });
+});
+
+module.exports = router;
